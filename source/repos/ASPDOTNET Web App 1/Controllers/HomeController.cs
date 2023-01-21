a1fdb283737ffbe16e67cfa79cd616efdff94d1b
cameron.compton@logicmonitor.com <cameron.compton@logicmonitor.com>
2023-01-21 10:22:07 -0600

    Working version instrumentation

---
+++ source/repos/ASPDOTNET Web App 1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPDOTNET_Web_App_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Modifying the ViewBag.Message in the HomeController.cs";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Modifying the ViewBag.Message in the HomeController.cs";
+
+            return View();
+        }
+        public ActionResult ViewPage1()
+        {
+            return View();
+        }
+        public ActionResult ViewPage2()
+        {
+            return View();
+        }
+    }
+}
---
+++ source/repos/ASPDOTNET Web App 1/Global.asax.cs
@@ -0,0 +1,41 @@
+﻿using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using OpenTelemetry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace ASPDOTNET_Web_App_1
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+
+        TracerProvider tracerProvider; 
+        protected void Application_Start()
+        {
+
+            Dictionary<string, object> tags = new Dictionary<string, object>();
+            tags.Add("host.name", Environment.MachineName.ToString());
+
+            tracerProvider = Sdk.CreateTracerProviderBuilder()
+               .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(System.Reflection.Assembly.GetExecutingAssembly().GetName().Name).AddAttributes(tags))
+               .AddAspNetInstrumentation()
+               .AddSqlClientInstrumentation()
+               .AddOtlpExporter(c =>
+               {
+                   c.Endpoint = new Uri("http://ip-172-31-22-215.us-east-2.compute.internal:4317");
+                   c.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
+               }
+               )
+               .Build();
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
